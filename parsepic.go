4a6b99eb03888e2ae20e740e31225b50acf1b2a0
eternal-flame-AD <eternal_flame-AD@protonmail.com>
2018-09-24 17:13:15 +0800

    add image/png support

--- parsepic.go
+++ parsepic.go
@@ -3,6 +3,7 @@ package flacpicture
 import (
 	"bytes"
 	"image/jpeg"
+	"image/png"
 )
 
 // ParsePicture decodes the image and inflated the Width, Height, ColorDepth, IndexedColorCount fields. This is called automatically by NewFromImageData
@@ -17,7 +18,17 @@ func (c *MetadataBlockPicture) ParsePicture() error {
 		size := img.Bounds()
 		c.Width = uint32(size.Max.X)
 		c.Height = uint32(size.Max.Y)
-		c.ColorDepth = uint32(8)
+		c.ColorDepth = uint32(24)
+	case "image/png":
+		img, err := png.Decode(bytes.NewReader(c.ImageData))
+		if err != nil {
+			return err
+		}
+		c.IndexedColorCount = uint32(0)
+		size := img.Bounds()
+		c.Width = uint32(size.Max.X)
+		c.Height = uint32(size.Max.Y)
+		c.ColorDepth = uint32(32)
 	default:
 		return ErrorUnsupportedMIME
 	}
--- picture_test.go
+++ picture_test.go
@@ -6,7 +6,34 @@ import (
 	httpclient "github.com/ddliu/go-httpclient"
 )
 
-func TestPictureDecode(t *testing.T) {
+func TestPNGPictureDecode(t *testing.T) {
+	imgres, err := httpclient.Begin().Get("https://upload.wikimedia.org/wikipedia/commons/4/47/PNG_transparency_demonstration_1.png")
+	if err != nil {
+		t.Errorf("Error while downloading test file: %s", err.Error())
+		t.FailNow()
+	}
+	imgdata, err := imgres.ReadAll()
+	if err != nil {
+		t.Errorf("Error while downloading test file: %s", err.Error())
+		t.FailNow()
+	}
+	pic, err := NewFromImageData(PictureTypeArtist, "test description", imgdata, "image/png")
+	if err != nil {
+		t.Errorf("Error while constructing image data: %s", err.Error())
+		t.Fail()
+	}
+
+	if pic.MIME != "image/png" {
+		t.Errorf("MIME decode error: got %s", pic.MIME)
+		t.Fail()
+	}
+
+	if pic.Height != 600 || pic.Width != 800 {
+		t.Errorf("JPEG size error: got %dx%d", pic.Width, pic.Height)
+		t.Fail()
+	}
+}
+func TestJPEGPictureDecode(t *testing.T) {
 	imgres, err := httpclient.Begin().Get("https://jpeg.org/images/jpeg-home.jpg")
 	if err != nil {
 		t.Errorf("Error while downloading test file: %s", err.Error())
