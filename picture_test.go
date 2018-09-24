1558a9b07c9fcac5c4aea23418660db034ce13b6
eternal-flame-AD <eternal_flame-AD@protonmail.com>
2018-09-24 17:27:14 +0800

    add parse existing flac

--- picture_test.go
+++ picture_test.go
@@ -1,9 +1,12 @@
 package flacpicture
 
 import (
+	"archive/zip"
+	"bytes"
 	"testing"
 
 	httpclient "github.com/ddliu/go-httpclient"
+	flac "github.com/go-flac/go-flac"
 )
 
 func TestPNGPictureDecode(t *testing.T) {
@@ -88,6 +91,59 @@ func TestPictureModify(t *testing.T) {
 
 	if pic.Description != "another description" {
 		t.Errorf("description unepected: %s", pic.Description)
+		t.Fail()
+	}
+}
+
+func TestJPEGFromExistingFLAC(t *testing.T) {
+	zipres, err := httpclient.Begin().Get("http://helpguide.sony.net/high-res/sample1/v1/data/Sample_BeeMoved_96kHz24bit.flac.zip")
+	if err != nil {
+		t.Errorf("Error while downloading test file: %s", err.Error())
+		t.FailNow()
+	}
+	zipdata, err := zipres.ReadAll()
+	if err != nil {
+		t.Errorf("Error while downloading test file: %s", err.Error())
+		t.FailNow()
+	}
+	zipfile, err := zip.NewReader(bytes.NewReader(zipdata), int64(len(zipdata)))
+	if err != nil {
+		t.Errorf("Error while decompressing test file: %s", err.Error())
+		t.FailNow()
+	}
+	if zipfile.File[0].Name != "Sample_BeeMoved_96kHz24bit.flac" {
+		t.Errorf("Unexpected test file content: %s", zipfile.File[0].Name)
+		t.FailNow()
+	}
+
+	flachandle, err := zipfile.File[0].Open()
+	if err != nil {
+		t.Errorf("Failed to decompress test file: %s", err)
+		t.FailNow()
+	}
+
+	f, err := flac.ParseBytes(flachandle)
+	if err != nil {
+		t.Errorf("Failed to parse flac file: %s", err)
+		t.FailNow()
+	}
+
+	var pic *MetadataBlockPicture
+	for _, meta := range f.Meta {
+		if meta.Type == flac.Picture {
+			pic, err = ParseFromMetaDataBlock(*meta)
+			if err != nil {
+				t.Errorf("Error while parsing metadata image: %s", err.Error())
+				t.Fail()
+			}
+		}
+	}
+	if pic.PictureType != PictureTypeFrontCover {
+		t.Error("Picture type does not match")
+		t.Fail()
+	}
+	if pic.MIME != "image/jpeg" {
+		t.Errorf("Picture MIME does not match: %s", pic.MIME)
 		t.Fail()
 	}
 }
